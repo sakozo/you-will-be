601998ec2760edafa4fe3b61a9d243ae21585bc8
sakozo <youlianggit@gmail.com>
2019-12-29 16:35:43 +0900

    自分で目標設定するための画面を追加

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -2,8 +2,16 @@ class GoalsController < ApplicationController
   def index
   end
 
-  def new
+  def show
     @goals = Goal.all
+  end
+
+  def new
+    @goal = Goal.new()
     #binding.pry
+  end
+
+  def create
+    
   end
 end
