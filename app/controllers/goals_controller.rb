f96f960b4dd775bb674394196f748647b4455097
sakozo <youlianggit@gmail.com>
2019-12-29 16:47:20 +0900

    目標が自分で登録できるようになった

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -8,10 +8,17 @@ class GoalsController < ApplicationController
 
   def new
     @goal = Goal.new()
-    #binding.pry
   end
 
   def create
-    
+    binding.pry
+    Goal.create(goal_params)
+    redirect_to edit_y_time_path(current_user.id)
+  end
+
+  
+  private
+  def goal_params
+    params.require(:goal).permit(:name, :time, :image)
   end
 end
