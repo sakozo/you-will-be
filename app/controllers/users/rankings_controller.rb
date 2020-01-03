d369ea017b2cd035652d75f31d16ba4b70a24b90
sakozo <youlianggit@gmail.com>
2020-01-03 14:38:41 +0900

    ランキングから個人ページに訪問

--- app/controllers/users/rankings_controller.rb
+++ app/controllers/users/rankings_controller.rb
@@ -1,6 +1,7 @@
 class Users::RankingsController < ApplicationController
   def index
     @user = User.find(current_user.id)
+    @goal = Goal.find(current_user.goal_id)
     #@same_goal_users = Goal.find(current_user.goal_id).users
 
     # @same_goal_users = Goal.find(current_user.goal_id).y_times.joins(:user).group(:user_id).sum(:time)
