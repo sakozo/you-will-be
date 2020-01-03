71af3f7adb72e64abb5c85ab69342f829a0de3b5
sakozo <youlianggit@gmail.com>
2020-01-03 14:15:47 +0900

    積み上げ時間の多いユーザー順に表示

--- app/controllers/users/rankings_controller.rb
+++ app/controllers/users/rankings_controller.rb
@@ -1,5 +1,10 @@
 class Users::RankingsController < ApplicationController
   def index
     @user = User.find(current_user.id)
+    #@same_goal_users = Goal.find(current_user.goal_id).users
+
+    # @same_goal_users = Goal.find(current_user.goal_id).y_times.joins(:user).group(:user_id).sum(:time)
+
+    @same_goal_users = Goal.find(current_user.goal_id).y_times.joins(:user).group(:user).sum(:time).sort {|(k1, v1), (k2, v2)| v2 <=> v1 }
   end
 end
