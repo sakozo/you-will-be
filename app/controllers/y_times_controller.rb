d19c8c8bd648b706a78d0e4d1075dde99583d7a2
sakozo <youlianggit@gmail.com>
2020-01-07 19:39:15 +0900

    個人ページscss

--- app/controllers/y_times_controller.rb
+++ app/controllers/y_times_controller.rb
@@ -4,8 +4,8 @@ class YTimesController < ApplicationController
     @user = User.find(current_user.id)        #ユーザー情報
     @goal = Goal.find(current_user.goal_id)   #ユーザーのgoal_idをみて目標を出力
     
-    @required_week = (@goal.time / @user.week_time).round
-    @required_month = (@required_week / 4).round
+    @required_week = (@goal.time / @user.week_time)
+    @required_month = (@required_week / 4)
 
     @achievement_date = (Date.current + (7 * @required_week)).strftime("%Y年%-m月%-d日")
     # @date = (Date.current - @user.created_at.to_date + 1).numerator 
