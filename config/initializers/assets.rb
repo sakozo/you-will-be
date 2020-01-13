eb39d6233c50411ee31aabc24ca6b72abafd628f
sakozo <youlianggit@gmail.com>
2020-01-13 15:55:26 +0900

    タブ切り替え追加

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,3 +11,4 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( chartkick.js )
 Rails.application.config.assets.precompile += %w( book/* )
+Rails.application.config.assets.precompile += %w( book/*.js )
