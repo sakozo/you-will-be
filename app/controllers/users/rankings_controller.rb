dd8cfeb2847fcd8449494f00746d2f9523a9523f
sakozo <youlianggit@gmail.com>
2020-01-19 22:18:19 +0900

    個別ランキングと全体ランキングの行き来をできるリンクを追加

--- app/controllers/users/rankings_controller.rb
+++ app/controllers/users/rankings_controller.rb
@@ -11,6 +11,7 @@ class Users::RankingsController < ApplicationController
 
   def show
     @user = User.find(current_user.id)
+    @goal = Goal.find(current_user.goal_id)
     @all_users = YTime.all.joins(:user).group(:user).sum(:time).sort {|(k1, v1), (k2, v2)| v2 <=> v1 }
   end
 end
