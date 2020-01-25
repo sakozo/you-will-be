323686fc94caa67b09481eb0caae273315a47a00
sakozo <youlianggit@gmail.com>
2020-01-25 13:40:52 +0900

    migrationエラー　解消のため

--- db/migrate/20191231100000_y_times.rb
+++ db/migrate/20191231100000_y_times.rb
@@ -1,10 +1,10 @@
-class YTimes < ActiveRecord::Migration[5.0]
-  def change
-    create_table :y_times do |t|
-      t.float :time, null: false
-      t.references :user,      null: false, foreign_key: true
-      t.references :goal,       null: false, foreign_key: true
-      t.timestamps
-    end
-  end
-end
+# class YTimes < ActiveRecord::Migration[5.0]
+#   def change
+#     create_table :y_times do |t|
+#       t.float :time, null: false
+#       t.references :user,      null: false, foreign_key: true
+#       t.references :goal,       null: false, foreign_key: true
+#       t.timestamps
+#     end
+#   end
+# end
