39531e59116c65307fb9a28de4d4dbbfdc47ccee
sakozo <youlianggit@gmail.com>
2020-01-26 10:39:35 +0900

    戻した

--- config/webpack/loaders/vue.js
+++ config/webpack/loaders/vue.js
@@ -1,27 +1,6 @@
-const VueLoaderPlugin = require('vue-loader/lib/plugin')
-
 module.exports = {
-  // ...
-  module: {
-    rules: [
-      {
-        test: /\.vue$/,
-        loader: "vue-loader"
-      }
-    ]
-  },
-
-  plugins: [
-    // ...
-    new VueLoaderPlugin()
-  ]
+  test: /\.vue(\.erb)?$/,
+  use: [{
+    loader: 'vue-loader'
+  }]
 }
-
-
-
-// module.exports = {
-//   test: /\.vue(\.erb)?$/,
-//   use: [{
-//     loader: 'vue-loader'
-//   }]
-// }
