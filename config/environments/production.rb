30b76590ae832966bd4b99a59c2b51f62250e92e
sakozo <youlianggit@gmail.com>
2020-01-26 10:40:33 +0900

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.webpacker.check_yarn_integrity = false
 end
